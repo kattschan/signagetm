513ffa950937d8ae441531df03c928c5d9689678
Kat Tschan <kat@kattschan.co.uk>
2023-11-06 21:11:16 +0000

    Add new slideshow

--- src/routes/get/+page.server.js
+++ src/routes/get/+page.server.js
@@ -5,7 +5,7 @@ export function load({ locals }) {
 	if (!locals.user) {
 		throw redirect(
 			307,
-			'https://docs.google.com/presentation/d/e/2PACX-1vQHOf-3fp5Dod1vQKqg62JXXrm1Z01hhs8C71oJAh0Pf6I4zlco2_dK29dm3aywLoGJ3YfA61JgCYSZ/embed?start=true&loop=true&delayms=10000'
+			'https://docs.google.com/presentation/d/e/2PACX-1vSpq6VJ96ky5ShT_HYfhXu5tuPSINIej3zWGjEmwYIfieyB9ilPVoRGVTBsXzmM8q1sdK6SW4MBta4m/pub?start=true&loop=true&delayms=30000'
 		);
 	}
 }
